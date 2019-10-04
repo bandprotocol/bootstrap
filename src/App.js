b659165eee298782792d450898ad823dc46eecec
prin-r <q@bandprotocol.com>
2019-10-04 12:44:41 +0700

    remove class

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import './App.css'
 
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
@@ -6,15 +6,11 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import LandingPage from 'pages/Landing'
 import NotFoundPage from 'pages/404'
 
-export default class App extends Component {
-  render() {
-    return (
-      <Router>
-        <Switch>
-          <Route exact path="/" component={LandingPage} />
-          <Route path="/" component={NotFoundPage} />
-        </Switch>
-      </Router>
-    )
-  }
-}
+export default () => (
+  <Router>
+    <Switch>
+      <Route exact path="/" component={LandingPage} />
+      <Route path="/" component={NotFoundPage} />
+    </Switch>
+  </Router>
+)
